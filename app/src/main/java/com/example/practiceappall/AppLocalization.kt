7774ab645a835505266a25eeb4e32f86e4b59e59
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-11-27 23:17:27 +0530

    Add

--- app/src/main/java/com/example/practiceappall/AppLocalization.kt
+++ app/src/main/java/com/example/practiceappall/AppLocalization.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Locale
 
 class AppLocalization : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,24 @@ class AppLocalization : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+//        val helloMessage = getString(R.string.app_name)
+//        val personalizedWelcome = getString(R.string.Text1)
+//
+//        println(helloMessage)          // Output: "Hello" (or "Hola" if the locale is Spanish)
+//        println(personalizedWelcome)   // Output: "Welcome to the app, Hrithik!" (or localized equivalent)
+
+
     }
+    /*fun setLocale(localeCode: String) {
+        val locale = Locale(localeCode)
+        Locale.setDefault(locale)
+
+        val config = resources.configuration
+        config.setLocale(locale)
+        resources.updateConfiguration(config, resources.displayMetrics)
+
+        // Restart activity to apply changes
+        recreate()
+    }*/
+
 }
