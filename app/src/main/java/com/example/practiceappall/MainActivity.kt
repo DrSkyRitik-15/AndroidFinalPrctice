b032014c9b356502de8e89aa86b3a483ed955f84
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-11-27 22:57:06 +0530

    Add

---
+++ app/src/main/java/com/example/practiceappall/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.practiceappall
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val bb = findViewById<Button>(R.id.Snake)
+
+        bb.setOnClickListener {
+
+            val kk = Snackbar.make(it,"My name is ritik",Snackbar.LENGTH_LONG).setAction("Action"){
+                Toast.makeText(this,"Click",Toast.LENGTH_LONG).show()
+            }
+            kk.show()
+
+        }
+    }
+}
