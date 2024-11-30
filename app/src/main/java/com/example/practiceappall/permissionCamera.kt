749c56dd4dd5e335b9b9c574d0e4ea4b07b8d3d1
DrSkyRitik-15 <ritiksinghkushwaha06@gmail.com>
2024-11-30 08:32:36 +0530

    add

---
+++ app/src/main/java/com/example/practiceappall/permissionCamera.kt
@@ -0,0 +1,70 @@
+package com.example.practiceappall
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class permissionCamera : AppCompatActivity() {
+    private var c=1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContentView(R.layout.activity_permission_camera)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btn = findViewById<Button>(R.id.btnnn)
+
+        btn.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_DENIED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    c
+                )
+            }
+
+        }
+
+    }
+
+
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==c)
+        {
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this,"Okokkkkkkkkkkk",Toast.LENGTH_LONG).show()
+            }
+            else
+            {
+                Toast.makeText(this,"Ozooooom",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
